7d78375f639fc79627182e24ab222c62cf0478e5
Srihari Radhakrishna <rsrihari95@gmail.com>
2018-10-13 21:15:32 +0530

    no changes

--- credable/users/models.py
+++ credable/users/models.py
@@ -6,7 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
 
-    # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
     credit_score = IntegerField()
